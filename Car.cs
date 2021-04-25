a36b1c2f14cc7bdaf1efadbca6eb4d9503bd2054
SabinaySh <you@example.com>
2021-04-25 03:52:50 -0700

    Writed Drive,AddFuel,LocalKm methods

---
+++ Car.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _25._04._21
+{
+    class Car
+    {
+        private double _fuelCapacity;
+        public double FuelCapacity => _fuelCapacity;
+
+        private double _fuelUsage;
+        public double FuelUsage => _fuelUsage;
+
+        private double _currentFuel;
+        public double CurrentFuel => _currentFuel;
+
+        private double _localKm;
+        public double LocalKm => _localKm;
+
+        private double _globalKm;
+        public double GlobalKm => _globalKm;
+
+
+        public Car(double FuelUsage, double FuelCapacity)
+        {
+            _fuelUsage = FuelUsage;
+            _fuelCapacity = FuelCapacity;
+
+            _currentFuel = _fuelCapacity * 0.2;
+        }
+
+        public bool Drive(double km)
+        {
+            if (km > _currentFuel / _fuelUsage)
+            {
+                return false;
+            }
+
+            _localKm += km;
+            _globalKm += km;
+
+            _currentFuel -= km * _fuelUsage;
+            return true;
+        }
+
+        public bool AddFuel(double litr)
+        {
+            if (_currentFuel + litr > _fuelCapacity)
+            {
+                return false;
+            }
+
+            _currentFuel += litr;
+            return true;
+        }
+
+        public bool AddFuel(int amount)
+        {
+            double litr = amount * 1.1;
+
+            if (_currentFuel + litr > _fuelCapacity)
+            {
+                return false;
+            }
+
+            _currentFuel += litr;
+            return true;
+        }
+
+        public void ResetLocal()
+        {
+            _localKm = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _25._04._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(0.12, 54);
+
+            car.AddFuel(12);
+
+            Console.WriteLine("Current Fuel: {0}, LocalKm: {1}, GlobalKm: {2}", car.CurrentFuel, car.LocalKm, car.GlobalKm);
+
+            Console.WriteLine(car.Drive(20));
+            Console.WriteLine(car.AddFuel(40));
+
+            Console.WriteLine("Current Fuel: {0}, LocalKm: {1}, GlobalKm: {2}", car.CurrentFuel, car.LocalKm, car.GlobalKm);
+
+            Console.WriteLine(car.Drive(150));
+            car.ResetLocal();
+
+            Console.WriteLine("Current Fuel: {0}, LocalKm: {1}, GlobalKm: {2}", car.CurrentFuel, car.LocalKm, car.GlobalKm);
+
+            Console.WriteLine(car.AddFuel(30));
+            Console.WriteLine(car.Drive(180));
+
+            Console.WriteLine("Son Current Fuel: {0}, LocalKm: {1}, GlobalKm: {2}", car.CurrentFuel, car.LocalKm, car.GlobalKm);
+        }
+    }
+}
